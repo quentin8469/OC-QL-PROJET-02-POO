f9e55b89579960df63f2df93f00dbe1ae8a077b0
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-03-31 21:43:38 +0200

    squelette_code

--- Class_Livres.py
+++ Class_Livres.py
@@ -1,1 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+import requests
+from bs4 import BeautifulSoup
+
+
+class Book:
+
+    """
+    Scrap un livre
+    """
+    
+    
+    def __init__(self, url_book):
+        """ docstring """
+		self.book_title = 
+		
+        
+    
+	
+	
+    def method(self):
+        """ docstring """
+        return
+
+
+    
+
+
+
+
+
+def main():
+    """ fonction principale, c'est ici que l'on appel les methodes???"""
+    
+    return
+
+
+if __name__ == '__main__':
+    main()
