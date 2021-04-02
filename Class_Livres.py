080341094f0f41108909fb4517ee7b9f2e84bef7
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-02 14:15:08 +0200

    test code classe

--- Class_Livres.py
+++ Class_Livres.py
@@ -10,32 +10,41 @@ class Book:
     """
     Scrap un livre
     """
-    
-    
-    def __init__(self, url_book):
+
+    def __init__(self, url_book, soupe):
         """ docstring """
-		self.book_title = 
-		
-        
-    
-	
-	
-    def method(self):
-        """ docstring """
-        return
 
+        self.url = url_book
+        self.soupe = soupe
+        self.upc = self.get_universal_product_code(url_book)
 
-    
+    def get_universal_product_code(self):
+        """ get UPC of a book """
 
-
-
+        upc = self.soupe.find_all("td")[0].text
+        return upc
 
 
 def main():
-    """ fonction principale, c'est ici que l'on appel les methodes???"""
-    
-    return
+    """ ddd """
+    url_book = (
+        "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+    )
+    html = requests.get(url_book)
+    print("bob1")
+    soupe = BeautifulSoup(html.content, "html.parser")
+    print("bob2")
+    livre = Book(url_book, soupe)
+    print("bob3")
+    livre.get_universal_product_code()
+    print("bob4")
+    return print(livre.get_universal_product_code())
 
 
-if __name__ == '__main__':
+print("bob5")
+
+
+"""
+if __name__ == "__main__":
     main()
+"""
